7d926848688de5dc5ba99aa00d66bf6650bb5bca
Maurice Götze <99mg24@web.de>
2025-03-04 17:26:16 +0100

    firefox test as driver. no binary location

---
+++ immo-saver/test_firefox.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options 
+#options = Options()
+#options.binary_location = r'/usr/bin/firefox-esr'
+#from selenium.webdriver.firefox.service import Service
+#service = Service('/home/pi/.local/bin/geckodriver')
+#driver = webdriver.Firefox(options=options)
+driver = webdriver.Firefox()
