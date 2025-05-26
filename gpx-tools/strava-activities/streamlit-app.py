b3612d06c6cb3b194f28fa86960eedb744d85dab
Maurice Götze <99mg24@web.de>
2025-05-26 19:40:47 +0200

    first streamlit test

--- gpx-tools/strava-activities/streamlit-app.py
+++ gpx-tools/strava-activities/streamlit-app.py
@@ -5,13 +5,21 @@ import time
 
 
 activities = getStravaData(True)
-
+st.success('Gathered Activities!', icon="✅")
 rides = activities.loc[activities['type'] == 'Ride']
 
+rides["distance"] = round(rides["distance"] / 1000,2)
+rides["average_speed"] = round(rides['average_speed'] * 3.6,2)
+rides["max_speed"] = round(rides['max_speed'] * 3.6,2)
 
-last_rows = np.random.randn(1, 1)
-chart = st.line_chart(last_rows)
 
+col1, col2, col3 = st.columns(3)
+with col1:
+    st.metric(label="Activity Count", value=len(rides))
+with col2:
+    st.metric(label="Total Distance", value=round(sum(rides["distance"]),2))
+with col3:
+    st.metric(label="Max Average Speed", value=round(max(rides['average_speed']),2))
 # for i in range(1, 101):
 #     new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)
 #     chart.add_rows(new_rows)
@@ -30,12 +38,15 @@ chart = st.line_chart(last_rows)
 # )
 # st.altair_chart(chart, use_container_width=True)
 
-st.bar_chart(data=rides, x='start_date_local', y='distance')
+st.bar_chart(data=rides, x='start_date_local',x_label='Datum', y='distance')
+st.bar_chart(data=rides, x='start_date_local',x_label='Datum', y='average_speed')
+st.bar_chart(data=rides, x='start_date_local',x_label='Datum', y='max_speed')
+st.bar_chart(data=rides, x='start_date_local',x_label='Datum', y='elapsed_time')
 
 #data = df.loc[countries]
 #data /= 1000000.0
 st.subheader("Bike Rides")
-st.dataframe(rides.sort_index(), use_container_width=True)
+st.dataframe(rides.sort_index())
 
 # Streamlit widgets automatically run the script from top to bottom. Since
 # this button is not connected to any other logic, it just causes a plain
