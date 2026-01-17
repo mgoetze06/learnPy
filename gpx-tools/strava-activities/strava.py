7746ea33b058b8bb800ba107030117ff442f979e
Maurice Götze <99mg24@web.de>
2026-01-17 11:56:06 +0100

    merged two scripts

--- gpx-tools/strava-activities/strava.py
+++ gpx-tools/strava-activities/strava.py
@@ -2,7 +2,7 @@ import requests
 import urllib3
 import pandas as pd
 from pandas import json_normalize
-from datetime import datetime
+from datetime import datetime,timedelta
 from strava_secrets_request import getFullHeaderWithAccessToken
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -10,7 +10,181 @@ import random
 import time
 import login
 import json
+import shutil
+import os
 from paho.mqtt import client as mqtt_client
+
+def logToFile(log):
+    f = open("log.txt", "a",encoding='UTF-8')
+    f.write(str(datetime.now()))
+    f.write(": ")
+    f.write(log + os.linesep)
+    f.close()
+
+def get_data_stream(activity_id,header):
+    api_url = f'https://www.strava.com/api/v3/activities/{activity_id}/streams'
+    query_params = 'time'
+    # for key, value in self.streams.items():
+    #     if value == 1:
+    #         query_params += f',{key}'
+    # url = f'{api_url}?keys={query_params}&key_by_type=true'
+    url = api_url + "?keys=time,latlng,altitude&key_by_type=true"
+
+    my_dataset = requests.get(url, headers=header).json()
+    return my_dataset
+
+def add_seconds_to_timestamp(start_timestamp, seconds):
+    start_time = getDatetimeFromTimestamp(start_timestamp)
+    new_time = start_time + timedelta(seconds=seconds)
+    return (new_time.isoformat() + "Z").replace("+00:00", "")
+
+def get_strava_activity(activity_id,header):
+        print("getting details for activity_id", activity_id)
+        api_url = 'https://www.strava.com/api/v3/activities/'
+        url = f'{api_url}{activity_id}?include_all_efforts=false'
+        print("requesting details from url", url)
+        data = requests.get(url, headers=header).json()
+        return data
+
+def getDatetimeFromTimestamp(timestamp):
+    replacedTimestamp = timestamp.replace("T"," ").replace("Z","")
+    time = datetime.strptime(f"{replacedTimestamp}", "%Y-%m-%d %H:%M:%S") 
+    #time = time + timedelta(hours=1)
+    return time
+
+def writeGPX(output,activity,data_streams):
+    print("writing activity to gpx file", output)
+    start_date = activity['start_date']
+
+    gpx_content_start = f'''<?xml version="1.0" encoding="UTF-8"?>
+<gpx xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd http://www.garmin.com/xmlschemas/GpxExtensions/v3 http://www.garmin.com/xmlschemas/GpxExtensionsv3.xsd http://www.garmin.com/xmlschemas/TrackPointExtension/v1 http://www.garmin.com/xmlschemas/TrackPointExtensionv1.xsd" creator="StravaGPX" version="1.1" xmlns="http://www.topografix.com/GPX/1/1" xmlns:gpxtpx="http://www.garmin.com/xmlschemas/TrackPointExtension/v1" xmlns:gpxx="http://www.garmin.com/xmlschemas/GpxExtensions/v3">
+ <metadata>
+  <time>{start_date}</time>
+ </metadata>
+ <trk>
+  <name>{activity['name']}</name>
+  <type>{activity['type']}</type>
+  <trkseg>'''
+    
+    gpx_content_end = '''
+  </trkseg>
+ </trk>
+</gpx>
+'''
+    if data_streams['latlng']['original_size'] != data_streams['time']['original_size']:
+        print("Error: latlng does not equal Time")
+        return
+
+    trkpts = []
+    for i in range(data_streams['time']['original_size']):
+        time = add_seconds_to_timestamp(activity['start_date'], data_streams['time']['data'][i])
+        trkpt = f'''
+   <trkpt lat="{float(data_streams['latlng']['data'][i][0]):.7f}" lon="{float(data_streams['latlng']['data'][i][1]):.7f}">
+    <ele>{float(data_streams['altitude']['data'][i]):.1f}</ele>
+    <time>{time}</time>
+   </trkpt>'''
+        trkpts.append(trkpt)
+#     <extensions>
+#      <gpxtpx:TrackPointExtension>
+# 
+#         trkpts.append(trkpt)
+
+# #         if self.streams['temp'] == 1:
+# #             trkpt = f'''      <gpxtpx:atemp>{data_streams['temp']['data'][i]}</gpxtpx:atemp>
+# # '''
+# #             trkpts.append(trkpt)
+# #         if self.streams['watts'] == 1:
+# #             trkpt = f'''      <gpxtpx:watts>{data_streams['watts']['data'][i]}</gpxtpx:watts>
+# # '''
+# #             trkpts.append(trkpt)
+# #         if self.streams['heartrate'] == 1:
+# #             trkpt = f'''      <gpxtpx:hr>{data_streams['heartrate']['data'][i]}</gpxtpx:hr>
+# # ''' 
+# #             trkpts.append(trkpt)
+
+# #         if self.streams['cadence'] == 1:
+# #             trkpt = f'''      <gpxtpx:cad>{data_streams['cadence']['data'][i]}</gpxtpx:cad>
+# # '''
+#         trkpts.append(trkpt)
+
+#         trkpt =f'''     </gpxtpx:TrackPointExtension>
+#     </extensions>
+        
+
+    with open(output, 'a') as f:
+        f.write(gpx_content_start)
+        f.write(''.join(trkpts))
+        f.write(gpx_content_end)
+
+    print('GPX file saved successfully.')
+
+def fileExistsHeatmapFolder(filename):
+    try:
+        destdir = "/home/boris/projects/gpx-heatmap/heatmap/gpx/"
+        fullPath = destdir + filename
+        return os.path.isfile(fullPath)
+    except:
+        return False
+    
+def fileExistsLocalFolder(filename):
+    try:
+        return os.path.isfile(filename)
+    except:
+        return False
+
+
+def copyGPXToHeatmapFolder(gpxfile):
+    try:
+        dest_dir = "/home/boris/projects/gpx-heatmap/heatmap/gpx"
+        shutil.copyfile(gpxfile,dest_dir)
+    except:
+        logToFile("copyGPXToHeatmapFolder failed")
+        pass
+
+def tryToRemoveFile(file):
+    try:
+        os.remove(file)
+    except:
+        logToFile("removing gpx file failed.")
+        pass
+
+def getActivityID():
+
+    activity_id = input("activity id:")
+    if len(activity_id) < 11:
+        return None
+    
+    if len(activity_id) == 11:
+        return activity_id
+    
+    try:
+        activity_id = activity_id.split("https://www.strava.com/activities/")[1].replace(" ","")
+        return activity_id
+    except:
+        pass
+
+
+def downloadGPXFileFromActivityID(activity_id=None):
+
+    header = getFullHeaderWithAccessToken()
+
+    if activity_id == None:
+        activity_id = getActivityID() #TODO check which activities already been downloaded and only get new ones
+    
+
+    activity = get_strava_activity(activity_id,header)
+    output = activity['start_date_local'].replace("T","-").replace("Z","").replace(":","-") + "_" + activity['name'].replace(" ","_").replace("/","")+".gpx"
+    
+    if(fileExistsHeatmapFolder(output)):
+        return
+    if(fileExistsLocalFolder(output)):
+        copyGPXToHeatmapFolder(output)
+        return
+    data_streams = get_data_stream(activity_id,header)
+    writeGPX(output,activity,data_streams)
+    copyGPXToHeatmapFolder(output)
+    tryToRemoveFile(output)
+
 
 def getStravaData():
     now = datetime.now()
@@ -45,10 +219,10 @@ def getStravaData():
             all_activities = my_dataset
 
         request_page_num += 1
-    print(len(all_activities))
-    for count, activity in enumerate(all_activities):
-        print(activity["name"])
-        print(count)
+    print("received total activities: ",len(all_activities))
+    # for count, activity in enumerate(all_activities):
+    #     print(activity["name"])
+    #     print(count)
 
 
 
@@ -147,16 +321,18 @@ def publish(client):
     return rides
 
 
-def downloadLastThreeActivities(rides):
-    for i in range(1,4):
+def downloadLastActivities(rides, downloadFiles):
+    upperrange = downloadFiles + 1
+    for i in range(1,upperrange):
         loc = i*-1
-        print(rides.iloc[loc])
-        print(rides.iloc[loc]["id"])
-        print(rides.iloc[loc]["upload_id"])
-        print(rides.iloc[loc]["upload_id_str"])
-        print(rides.iloc[loc]["external_id"])
-
-
+        #print(rides.iloc[loc])
+        id = rides.iloc[loc]["id"]
+        print("trying to download ride with id: ",id)
+        try:
+            downloadGPXFileFromActivityID(id)
+        except:
+            print("error downloadin ride with id: ",id)
+            pass
     return 
 
 
@@ -167,7 +343,7 @@ def run():
         rides = publish(client)
         client.loop_stop()
 
-        downloadLastThreeActivities(rides)
+        downloadLastActivities(rides,3)
         print("entering sleep")
         time.sleep(60*60*6)
         #time.sleep(20)
